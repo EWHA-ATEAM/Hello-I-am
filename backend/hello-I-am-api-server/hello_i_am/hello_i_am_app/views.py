cedb0cd4b422692614341afdcd4fc5333c1acee2
cje1903 <cje1396@gmail.com>
2022-03-16 18:00:33 +0900

    [chore] change image classifier model
    train 사진 수, epoch 를 늘려서 학습시켜본 모델로 교체

--- backend/hello-I-am-api-server/hello_i_am/hello_i_am_app/views.py
+++ backend/hello-I-am-api-server/hello_i_am/hello_i_am_app/views.py
@@ -9,13 +9,48 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text
 
+from tensorflow.keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+
+
+## 손동작 사진 인삭하는 함수
 @api_view(['POST'])
 def get_image(request):
-    image =(request.data.get('screenImage'))
-    print(image)
-    print(type(image))
-    return HttpResponse(17)
 
+    # 유니티로부터 이미지 받아오기
+    screenimage = (request.data.get('screenImage'))
+    print(screenimage)
+    print(type(screenimage))
+
+    model = load_model('model2.h5')    # 사진 인식 모델 로드하기
+
+    # 모델에 인풋할 수 있도록 형태 바꾸기
+    img = image.load_img(screenimage, target_size=(150, 150))
+    img2 = image.img_to_array(img)
+    img2 = np.expand_dims(img2, axis=0)
+
+    # 예측하기
+    predict = model.predict(img2)
+    print("사진의 예측값은:", predict)
+    # {'heart':0, 'hi':1, 'pet':2}
+
+    res = -1    # 아무 인덱스도 아닌 -1로 초기화
+
+    if predict[0][0] == 1:
+        print('heart')
+        res = 0
+    elif predict[0][1] == 1:
+        print('hi')
+        res = 1
+    elif predict[0][2] == 1:
+        print('pet')
+        res = 2
+
+    return HttpResponse(res)
+
+
+## 음성인식으로 받은 문장의 카테고리를 분류해서 대답하는 함수
 @api_view(['POST'])
 def label_user_chat(request):
     sentence = str(request.data.get('sentence'))
@@ -34,10 +69,10 @@ def label_user_chat(request):
 
     # Encode sentences using distilkobert_sentence_encoder
     sentences1 = tf.constant([
-    sentence
+        sentence
     ])
     sentences2 = tf.constant([  # 질문의 카테고리에 따른 대표 질문들
-    "안녕? 너는 이름이 뭐야","너는 어디에 살아?", "밥 뭐먹었어?", "기분이 어때?", "힘든 이유가 뭐야?", "내가 널 도울 방법이 있을까?",
+        "안녕? 너는 이름이 뭐야", "너는 어디에 살아?", "밥 뭐먹었어?", "기분이 어때?", "힘든 이유가 뭐야?", "내가 널 도울 방법이 있을까?",
         "너는 누구랑 같이 살아?", "너는 몇 살이야?", "너의 몸무게는 얼마야?"
     ])
     embeddings1 = model(sentences1)

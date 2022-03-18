429cac15e36dbe2014843e3d86967b0ae9ae87d0
cje1903 <cje1396@gmail.com>
2022-03-18 18:29:44 +0900

    [feat] put image to classifier model
    - 유니티에서 받은 이미지 인식 모델에 넣기 (db에 잠시 저장 후 삭제)
    - 동물 종류 판별하여 답변 전송 (동물 종류 추가 (기존 1개 -> 3개))

--- backend/hello-I-am-api-server/hello_i_am/hello_i_am_app/views.py
+++ backend/hello-I-am-api-server/hello_i_am/hello_i_am_app/views.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -9,26 +11,45 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text
 
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
 
+from .models import screenimage_delete_action, Screenimage
+
+import matplotlib.pyplot as plt
 
 ## 손동작 사진 인삭하는 함수
 @api_view(['POST'])
 def get_image(request):
+    form = Screenimage()
 
     # 유니티로부터 이미지 받아오기
-    screenimage = (request.data.get('screenImage'))
+    screenimage = request.FILES['screenImage']
     print(screenimage)
-    print(type(screenimage))
+    form.image = screenimage
+    form.save()
 
-    model = load_model('model2.h5')    # 사진 인식 모델 로드하기
+    # a = imggg.imread(screenimage)
+    # plt.imshow(a)
+    # plt.show()
+
+    # 사진 인식 모델 로드하기
+    file_name = os.path.dirname(__file__) + '/model2.h5'    # 배포할 때를 대비해... (내 로컬 절대경로로 쓰면 안 됨)
+    model = load_model(file_name)
 
     # 모델에 인풋할 수 있도록 형태 바꾸기
-    img = image.load_img(screenimage, target_size=(150, 150))
+    img = image.load_img("./media/images/screenImage.dat", target_size=(150, 150))
+    print(os.listdir("./media/images/"))
+    plt.imshow(img)
+    plt.show()
+
     img2 = image.img_to_array(img)
     img2 = np.expand_dims(img2, axis=0)
+
+    print('사진 삭제할게요')
+    form.delete()  # db에서 삭제하기
+    # media 폴더에서도 삭제하기 (screenimage_delete_action)
 
     # 예측하기
     predict = model.predict(img2)
@@ -37,15 +58,19 @@ def get_image(request):
 
     res = -1    # 아무 인덱스도 아닌 -1로 초기화
 
-    if predict[0][0] == 1:
-        print('heart')
-        res = 0
-    elif predict[0][1] == 1:
-        print('hi')
-        res = 1
-    elif predict[0][2] == 1:
-        print('pet')
-        res = 2
+    print(predict[0][0])
+    print(predict[0][1])
+    print(predict[0][2])
+
+    # if predict[0][0] == 1:
+    #     print('heart')
+    #     res = 0
+    # elif predict[0][1] == 1:
+    #     print('hi')
+    #     res = 1
+    # elif predict[0][2] == 1:
+    #     print('pet')
+    #     res = 2
 
     return HttpResponse(res)
 
@@ -53,8 +78,10 @@ def get_image(request):
 ## 음성인식으로 받은 문장의 카테고리를 분류해서 대답하는 함수
 @api_view(['POST'])
 def label_user_chat(request):
-    sentence = str(request.data.get('sentence'))
+    # sentence = str(request.data.get('sentence'))
+    sentence = str(request.POST['sentence'])
     print(sentence)
+    animal = int(request.POST['animal'])
 
     # Load required models
     encoder = hub.KerasLayer("https://tfhub.dev/jeongukjae/distilkobert_sentence_encoder/1")
@@ -83,25 +110,73 @@ def label_user_chat(request):
     res_index = np.argmax(res[0])
 
     # 질문의 카테고리에 따른 대답
-    if res_index == 0:
-        res = "(0) 안녕 나는 곰곰이야!"
-    elif res_index == 1:
-        res = "(1) 나는 북극에 살아"
-    elif res_index == 2:
-        res = "(2) 나는 바다사자를 먹었어"
-    elif res_index == 3:
-        res = "(3) 나는 지금 우울해"
-    elif res_index == 4:
-        res = "(4) 북극의 빙하가 녹고 있어서 살 곳이 없어지고 있어.."
-    elif res_index == 5:
-        res = "나를 돕기 위해서는 지구를 아껴줘"
-    elif res_index == 6:
-        res = "얼마전에 독립해서 혼자 살고 있어"
-    elif res_index == 7:
-        res = "나는 4살이야"
-    elif res_index == 8:
-        res = "나느 400kg 정도 돼"
-    else:
-        res = "나머지^^"
+    # 북극곰 (animal: 0)
+    if animal == 0:
+        if res_index == 0:
+            res = "북극곰(0)"
+        elif res_index == 1:
+            res = "북극곰(1)"
+        elif res_index == 2:
+            res = "북극곰(2)"
+        elif res_index == 3:
+            res = "북극곰(3)"
+        elif res_index == 4:
+            res = "북극곰(4)"
+        elif res_index == 5:
+            res = "북극곰(5)"
+        elif res_index == 6:
+            res = "북극곰(6)"
+        elif res_index == 7:
+            res = "북극곰(7)"
+        elif res_index == 8:
+            res = "북극곰(8)"
+        else:
+            res = "나머지^^"
+
+    # 레서판다 (animal: 1)
+    if animal == 1:
+        if res_index == 0:
+            res = "레서판다(0)"
+        elif res_index == 1:
+            res = "레서판다(1)"
+        elif res_index == 2:
+            res = "레서판다(2)"
+        elif res_index == 3:
+            res = "레서판다(3)"
+        elif res_index == 4:
+            res = "레서판다(4)"
+        elif res_index == 5:
+            res = "레서판다(5)"
+        elif res_index == 6:
+            res = "레서판다(6)"
+        elif res_index == 7:
+            res = "레서판다(7)"
+        elif res_index == 8:
+            res = "레서판다(8)"
+        else:
+            res = "나머지^^"
+
+    # 설표 (animal: 2)
+    if animal == 2:
+        if res_index == 0:
+            res = "설표(0)"
+        elif res_index == 1:
+            res = "설표(1)"
+        elif res_index == 2:
+            res = "설표(2)"
+        elif res_index == 3:
+            res = "설표(3)"
+        elif res_index == 4:
+            res = "설표(4)"
+        elif res_index == 5:
+            res = "설표(5)"
+        elif res_index == 6:
+            res = "설표(6)"
+        elif res_index == 7:
+            res = "설표(7)"
+        elif res_index == 8:
+            res = "설표(8)"
+        else:
+            res = "나머지^^"
 
     return HttpResponse(res)

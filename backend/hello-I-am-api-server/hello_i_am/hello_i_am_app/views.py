af8acf6e7096970ef1cbf59a895b16bbd7b239ca
cje1903 <cje1903@naver.com>
2022-03-23 21:26:02 +0900

    [chore] simplify code

--- backend/hello-I-am-api-server/hello_i_am/hello_i_am_app/views.py
+++ backend/hello-I-am-api-server/hello_i_am/hello_i_am_app/views.py
@@ -15,9 +15,7 @@ from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
 
-from .models import screenimage_delete_action, Screenimage
-
-import matplotlib.pyplot as plt
+from .models import Screenimage
 
 ## 손동작 사진 인삭하는 함수
 @api_view(['POST'])
@@ -30,10 +28,6 @@ def get_image(request):
     form.image = screenimage
     form.save()
 
-    # a = imggg.imread(screenimage)
-    # plt.imshow(a)
-    # plt.show()
-
     # 사진 인식 모델 로드하기
     file_name = os.path.dirname(__file__) + '/model2.h5'    # 배포할 때를 대비해... (내 로컬 절대경로로 쓰면 안 됨)
     model = load_model(file_name)
@@ -41,15 +35,12 @@ def get_image(request):
     # 모델에 인풋할 수 있도록 형태 바꾸기
     img = image.load_img("./media/images/screenImage.dat", target_size=(150, 150))
     print(os.listdir("./media/images/"))
-    plt.imshow(img)
-    plt.show()
 
     img2 = image.img_to_array(img)
     img2 = np.expand_dims(img2, axis=0)
 
     print('사진 삭제할게요')
-    form.delete()  # db에서 삭제하기
-    # media 폴더에서도 삭제하기 (screenimage_delete_action)
+    form.delete()  # db에서 삭제하기 - media 폴더의 사진도 같이 삭제
 
     # 예측하기
     predict = model.predict(img2)
@@ -58,19 +49,12 @@ def get_image(request):
 
     res = -1    # 아무 인덱스도 아닌 -1로 초기화
 
-    print(predict[0][0])
-    print(predict[0][1])
-    print(predict[0][2])
-
-    # if predict[0][0] == 1:
-    #     print('heart')
-    #     res = 0
-    # elif predict[0][1] == 1:
-    #     print('hi')
-    #     res = 1
-    # elif predict[0][2] == 1:
-    #     print('pet')
-    #     res = 2
+    if predict[0][0] == 1:
+        res = 0
+    elif predict[0][1] == 1:
+        res = 1
+    elif predict[0][2] == 1:
+        res = 2
 
     return HttpResponse(res)
 
@@ -109,74 +93,20 @@ def label_user_chat(request):
     res = tf.tensordot(embeddings1, embeddings2, axes=[[1], [1]])
     res_index = np.argmax(res[0])
 
+    polarbear_res = ["북극곰(0)", "북극곰(1)", "북극곰(2)", "북극곰(3)", "북극곰(4)", "북극곰(5)", "북극곰(6)", "북극곰(7)", "북극곰(8)"]
+    redpanda_res = ["레서판다(0)", "레서판다(1)", "레서판다(2)", "레서판다(3)", "레서판다(4)", "레서판다(5)", "레서판다(6)", "레서판다(7)", "레서판다(8)"]
+    snowleopard_res = ["설표(0)", "설표(1)", "설표(2)", "설표(3)", "설표(4)", "설표(5)", "설표(6)", "설표(7)", "설표(8)"]
+
     # 질문의 카테고리에 따른 대답
     # 북극곰 (animal: 0)
     if animal == 0:
-        if res_index == 0:
-            res = "북극곰(0)"
-        elif res_index == 1:
-            res = "북극곰(1)"
-        elif res_index == 2:
-            res = "북극곰(2)"
-        elif res_index == 3:
-            res = "북극곰(3)"
-        elif res_index == 4:
-            res = "북극곰(4)"
-        elif res_index == 5:
-            res = "북극곰(5)"
-        elif res_index == 6:
-            res = "북극곰(6)"
-        elif res_index == 7:
-            res = "북극곰(7)"
-        elif res_index == 8:
-            res = "북극곰(8)"
-        else:
-            res = "나머지^^"
+        res = polarbear_res[res_index]
 
     # 레서판다 (animal: 1)
     if animal == 1:
-        if res_index == 0:
-            res = "레서판다(0)"
-        elif res_index == 1:
-            res = "레서판다(1)"
-        elif res_index == 2:
-            res = "레서판다(2)"
-        elif res_index == 3:
-            res = "레서판다(3)"
-        elif res_index == 4:
-            res = "레서판다(4)"
-        elif res_index == 5:
-            res = "레서판다(5)"
-        elif res_index == 6:
-            res = "레서판다(6)"
-        elif res_index == 7:
-            res = "레서판다(7)"
-        elif res_index == 8:
-            res = "레서판다(8)"
-        else:
-            res = "나머지^^"
-
+        res = redpanda_res[res_index]
     # 설표 (animal: 2)
     if animal == 2:
-        if res_index == 0:
-            res = "설표(0)"
-        elif res_index == 1:
-            res = "설표(1)"
-        elif res_index == 2:
-            res = "설표(2)"
-        elif res_index == 3:
-            res = "설표(3)"
-        elif res_index == 4:
-            res = "설표(4)"
-        elif res_index == 5:
-            res = "설표(5)"
-        elif res_index == 6:
-            res = "설표(6)"
-        elif res_index == 7:
-            res = "설표(7)"
-        elif res_index == 8:
-            res = "설표(8)"
-        else:
-            res = "나머지^^"
+        res = snowleopard_res[res_index]
 
     return HttpResponse(res)

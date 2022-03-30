41845b9b7869fc953f86b346cf5fbec99d989c82
zmin9 <o3o0903@gmail.com>
2022-03-30 20:39:06 +0900

    [feat] add random anim every 10sec
    - 랜덤으로 애니메이션 재생
    - 테스트는 아직 못함

--- frontend/Hello_I_am/Assets/Scripts/Backend/AnimalAnimation.cs
+++ frontend/Hello_I_am/Assets/Scripts/Backend/AnimalAnimation.cs
@@ -6,10 +6,21 @@ public class AnimalAnimation : MonoBehaviour
     private ServerCommunicate serverCommunicate;
     private Animator animal;
 
+    private float timer;
+    private float intervalTime = 10.0f;
+
+    private int maxAnimNum;
+
     private void Start()
     {
+        timer = 0.0f;
         serverCommunicate = GameObject.Find("Canvas").GetComponent<ServerCommunicate>();
         animal = GetComponent<Animator>();
+
+        // 랜덤 애니메이션을 위한 최대 애니메이션 값 추가
+        if (AppManager.instance.animal_index == 0) maxAnimNum = 4;  // polarbear
+        else if (AppManager.instance.animal_index == 0) maxAnimNum = 6; // redpanda
+        else if (AppManager.instance.animal_index == 0) maxAnimNum = 3; // snow leopard
     }
 
     private void Update()
@@ -35,6 +46,18 @@ public class AnimalAnimation : MonoBehaviour
         else
         {
             // 랜덤으로 애니메이션 재생되면 재밌을듯
+            if (animal.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+            {
+                timer += Time.deltaTime;
+
+                if (timer > intervalTime)
+                {
+                    timer = 0;
+
+                    string animName = "randomTrigger" + Random.Range(0, maxAnimNum);
+                    animal.SetTrigger(animName);
+                }
+            }
         }
     }
 
